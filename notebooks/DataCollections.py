f17102a3d7c014d422efc01626a489d664d50e61
Devbrat Anuragi <39759685+AslanDevbrat@users.noreply.github.com>
2021-07-04 20:00:49 +0530

    created the folder for data collection. Need to collect data

--- notebooks/DataCollections.py
+++ notebooks/DataCollections.py
@@ -1,10 +1,8 @@
-# import Dependendencies
-import cv2
-import numpy as np
 import os
-from matplotlib import pyplot as ptl
-import time
+import numpy as np
+import cv2
 import mediapipe as mp
+
 
 # Key points using MP Holistics
 
@@ -13,6 +11,26 @@ mp_holistic = mp.solutions.holistic
 # Drawing Utilities
 mp_drawing = mp.solutions.drawing_utils
 
+
+
+# path for exported data, numpy arrays
+DATA_PATH = os.path.join('data/MP_DATA')
+
+# Actions that we try to detect
+actions = np.array(['hello', 'thanks', 'iloveyou'])
+
+# Thirty video of data
+no_sequences = 30
+
+# Videos are going to be 30 fps
+sequence_length = 30
+
+for action in actions:
+    for sequence in range(no_sequences):
+        try:
+            os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
+        except:
+            pass
 
 def mediapipe_detection(image, model):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # color conversion
@@ -23,7 +41,7 @@ def mediapipe_detection(image, model):
     return image, results
 
 
-def draw_landmark(image, results):
+def draw_landmarks(image, results):
     mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS,
                               mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
                               mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1))
@@ -54,22 +72,50 @@ def extract_keypoints(results):
     return np.concatenate([pose, face, lh, rh])
 
 
-
 cap = cv2.VideoCapture(0)
-with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)as holistic:
-    while cap.isOpened():
-        ret, frame = cap.read()
+# Set mediapipe model
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    # NEW LOOP
+    # Loop through actions
+    for action in actions:
+        # Loop through sequences aka videos
+        for sequence in range(no_sequences):
+            # Loop through video length aka sequence length
+            for frame_num in range(sequence_length):
 
-        image, results = mediapipe_detection(frame, holistic)
-        # dprint(results.pose_landmarks)
-        # draw landmark
-        draw_landmark(image, results)
-        x = extract_keypoints(results)
-        print(x)
-        cv2.imshow('OpenCv Feed', image)
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
+                # Read feed
+                ret, frame = cap.read()
+
+                # Make detections
+                image, results = mediapipe_detection(frame, holistic)
+                #                 print(results)
+
+                # Draw landmarks
+                draw_landmarks(image, results)
+
+                # NEW Apply wait logic
+                if frame_num == 0:
+                    cv2.putText(image, 'STARTING COLLECTION', (120, 200),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4, cv2.LINE_AA)
+                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15, 12),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                    # Show to screen
+                    cv2.imshow('OpenCV Feed', image)
+                    cv2.waitKey(2000)
+                else:
+                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15, 12),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                    # Show to screen
+                    cv2.imshow('OpenCV Feed', image)
+
+                # NEW Export keypoints
+                keypoints = extract_keypoints(results)
+                npy_path = os.path.join(DATA_PATH, action, str(sequence), str(frame_num))
+                np.save(npy_path, keypoints)
+
+                # Break gracefully
+                if cv2.waitKey(10) & 0xFF == ord('q'):
+                    break
 
     cap.release()
     cv2.destroyAllWindows()
-# print(x)
