8a7e7ee16db3e412c67cba8cbc6e0a244c7a6481
Devbrat Anuragi <39759685+AslanDevbrat@users.noreply.github.com>
2021-07-05 20:32:21 +0530

    implemented the real time application

--- notebooks/SOSsignalDetection.py
+++ notebooks/SOSsignalDetection.py
@@ -6,12 +6,15 @@ import os
 from matplotlib import pyplot as ptl
 import time
 import mediapipe as mp
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.callbacks import TensorBoard
 #from DataCollections import DATA_PATH
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
 #from LoadData import load_data
 # Key points using MP Holistics
-
+from tensorflow.keras.models import Sequential
 # Holistic model
 mp_holistic = mp.solutions.holistic
 # Drawing Utilities
@@ -85,23 +88,93 @@ X, Y = load_data()
 y = to_categorical(Y).astype(int)
 
 
+sequence =[]
+setetence = []
+threshold = 0.4
+model = Sequential()
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,1662)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))
 
+model.load_weights('E:\\9th sem\\MS_Thesis\\Awiros\\Project\\models\\action.h5')
 
 cap = cv2.VideoCapture(0)
-with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)as holistic:
+i=0
+
+colors = [(245, 117, 16), (117, 245, 16), (16, 117, 245)]
+
+
+def prob_viz(res, actions, input_frame, colors):
+    output_frame = input_frame.copy()
+    for num, prob in enumerate(res):
+        cv2.rectangle(output_frame, (0, 60 + num * 40), (int(prob * 100), 90 + num * 40), colors[num], -1)
+        cv2.putText(output_frame, actions[num], (0, 85 + num * 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
+                    cv2.LINE_AA)
+
+    return output_frame
+
+
+# 1. New detection variables
+sequence = []
+sentence = []
+predictions = []
+threshold = 0.8
+
+cap = cv2.VideoCapture(0)
+# Set mediapipe model
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     while cap.isOpened():
+
+        # Read feed
         ret, frame = cap.read()
 
+        # Make detections
         image, results = mediapipe_detection(frame, holistic)
-        # dprint(results.pose_landmarks)
-        # draw landmark
+        print(results)
+
+        # Draw landmarks
         draw_landmark(image, results)
-        x = extract_keypoints(results)
-        print(x)
-        cv2.imshow('OpenCv Feed', image)
+
+        # 2. Prediction logic
+        keypoints = extract_keypoints(results)
+        sequence.append(keypoints)
+        sequence = sequence[-30:]
+
+        if len(sequence) == 30:
+            res = model.predict(np.expand_dims(sequence, axis=0))[0]
+            print(actions[np.argmax(res)])
+            predictions.append(np.argmax(res))
+
+            # 3. Viz logic
+            if np.unique(predictions[-10:])[0] == np.argmax(res):
+                if res[np.argmax(res)] > threshold:
+
+                    if len(sentence) > 0:
+                        if actions[np.argmax(res)] != sentence[-1]:
+                            sentence.append(actions[np.argmax(res)])
+                    else:
+                        sentence.append(actions[np.argmax(res)])
+
+            if len(sentence) > 5:
+                sentence = sentence[-5:]
+
+            # Viz probabilities
+            image = prob_viz(res, actions, image, colors)
+
+        cv2.rectangle(image, (0, 0), (640, 40), (245, 117, 16), -1)
+        cv2.putText(image, ' '.join(sentence), (3, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+        # Show to screen
+        cv2.imshow('OpenCV Feed', image)
+
+        # Break gracefully
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
-
     cap.release()
     cv2.destroyAllWindows()
 # print(x)
